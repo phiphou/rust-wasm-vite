e39f253b95a3d1ab0ea3d39931fea18fcb25609e
Phiphou <phiphou@phiphou.com>
2025-02-23 12:13:21 +0100

    test 5

--- game_of_life/build-wasm.ps1
+++ game_of_life/build-wasm.ps1
@@ -1,7 +1,7 @@
 # Construire le projet WebAssembly
 cargo build --release --features wasm
 
-wasm-pack build --target bundler  --features wasm
+wasm-pack build --target web  --features wasm
 
 # Copier le dossier pkg vers ton répertoire frontend (avec remplacement forcé)
 Copy-Item -Recurse -Force -Path .\pkg -Destination ..\game_of_life_web\
--- game_of_life/build-wasm.sh
+++ game_of_life/build-wasm.sh
@@ -3,9 +3,7 @@
 # Construire le projet WebAssembly
 cargo build --release --features wasm
 
-wasm-pack build --target bundler  --features wasm
+wasm-pack build --target web  --features wasm
 
 # Copier le dossier pkg vers le frontend
 cp -r ./pkg ../game_of_life_web
-
-
